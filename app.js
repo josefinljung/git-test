c73c1886dfa831b0fd47c60620360e82314ae784
Emma Teo <emmateo@Emmas-MBP-3.mediegruppen.se>
2020-01-21 13:48:45 +0100

    här får du lite kod

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 console.log("Hej från Git");
 console.log("Hejdå från Emma<3")
+
+
+const express = require ('express');
+const app = express();
+app.get('/', (req,res) => {res.send("Hejsanhoppsan")})
+app.listen(3000, ()=>console.log('Server redo'));
